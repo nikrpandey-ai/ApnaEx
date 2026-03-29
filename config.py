e2e8740d2e542b8b14ae7cc8dfa27ae609830304
nikrpandey-ai <nikrpandey@gmail.com>
2026-03-29 23:37:38 +0530

    Update CHANNEL_ID2 environment variable value

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1997693424"))
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003713592223"))
 
 # //FORCE_CHANNEL_ID
-CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1003713592223")) 
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002195125473")) 
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://besib69802:YMOfgvnyjbRgW5qt@cluster0.yzzu2gn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # -----------------------------------------------
